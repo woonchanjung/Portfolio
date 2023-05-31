2eb087204d9d61bf6802cd4b43b5814231c8934a
xkrzypandax <xkrzypandax@gmail.com>
2023-05-30 21:01:52 -0700

    Data

--- src/data.js
+++ src/data.js
@@ -41,13 +41,6 @@ export const testimonials = [
     name: "Jesse Hicks",
     company: "Zoozle",
   },
-  {
-    quote:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
-    image: "https://randomuser.me/api/portraits/men/94.jpg",
-    name: "Ruben Alvarez",
-    company: "Dooble.io",
-  },
 ];
 
 export const skills = [
