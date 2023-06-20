3e1d894234cf798d2e72f0de1c0572dfcb676fb0
xkrzypandax <xkrzypandax@gmail.com>
2023-06-19 20:45:08 -0700

    Data

--- src/data.js
+++ src/data.js
@@ -25,7 +25,7 @@ export const projects = [
   },
   {
     title: "JavaScript Quiz Game",
-    subtitle: "React Hooks",
+    subtitle: "HTML, CSS, JavaScript",
     description:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
     image: "./project-4.gif",
