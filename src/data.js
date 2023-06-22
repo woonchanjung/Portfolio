52456acdba2a703193dfeb174533e6e1c09df284
xkrzypandax <xkrzypandax@gmail.com>
2023-06-21 23:47:38 -0700

    Testimonials

--- src/data.js
+++ src/data.js
@@ -1,55 +1,59 @@
 export const projects = [
-  {
-    title: "L'Art du Style",
-    subtitle: "Python, Django, PostgreSQL, Tailwind, Heroku",
-    description:
-      "Developed L’Art du Style, a Python and Django-based application aimed at empowering individuals to refine their personal style by organizing and seamlessly matching their wardrobe.",
-    image: "./project-1.gif",
-    link: "https://l-art-du-style.herokuapp.com/",
-  },
-  {
-    title: "FindResGo",
-    subtitle: "MERN stack (MongoDB, Express.js, React.js, Node.js), Tailwind CSS, Heroku",
-    description:
-      "",
-    image: "./project-2.gif",
-    link: "https://findresgo.herokuapp.com/",
-  },
-  {
-    title: "PokeMarket",
-    subtitle: "MEN stack (MongoDB, Express.js, Node.js)",
-    description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-    image: "./project-3.gif",
-    link: "https://pokemarket.herokuapp.com/pokemon",
-  },
-  {
-    title: "JavaScript Quiz Game",
-    subtitle: "HTML, CSS, JavaScript",
-    description:
-      "",
-    image: "./project-4.gif",
-    link: "https://ryanqle.github.io/SimonSays/",
-  },
+    {
+        title: "L'Art du Style",
+        subtitle: "Python, Django, PostgreSQL, Tailwind, Heroku",
+        description:
+            "Developed L’Art du Style, a Python and Django-based application aimed at empowering individuals to refine their personal style by organizing and seamlessly matching their wardrobe.",
+        image: "./project-1.gif",
+        link: "https://l-art-du-style.herokuapp.com/",
+    },
+    {
+        title: "FindResGo",
+        subtitle:
+            "MERN stack (MongoDB, Express.js, React.js, Node.js), Tailwind CSS, Heroku",
+        description: "",
+        image: "./project-2.gif",
+        link: "https://findresgo.herokuapp.com/",
+    },
+    {
+        title: "PokeMarket",
+        subtitle: "MEN stack (MongoDB, Express.js, Node.js)",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-3.gif",
+        link: "https://pokemarket.herokuapp.com/pokemon",
+    },
+    {
+        title: "JavaScript Quiz Game",
+        subtitle: "HTML, CSS, JavaScript",
+        description: "",
+        image: "./project-4.gif",
+        link: "https://ryanqle.github.io/SimonSays/",
+    },
 ];
 
 export const testimonials = [
-  {
-    quote:
-      "",
-    image: "https://randomuser.me/api/portraits/men/1.jpg",
-    name: "Jesse Hicks",
-    company: "Zoozle",
-  },
+    {
+        quote: "",
+        image: "https://randomuser.me/api/portraits/men/1.jpg",
+        name: "Jesse Hicks",
+        company: "Zoozle",
+    },
+    {
+        quote: "",
+        image: "https://randomuser.me/api/portraits/men/1.jpg",
+        name: "Jesse Hicks",
+        company: "Zoozle",
+    },
 ];
 
 export const skills = [
-  "JavaScript",
-  "React",
-  "Next.js",
-  "Node",
-  "GraphQL",
-  "Material UI",
-  "Python",
-  "HTML",
+    "JavaScript",
+    "React",
+    "Next.js",
+    "Node",
+    "GraphQL",
+    "Material UI",
+    "Python",
+    "HTML",
 ];
