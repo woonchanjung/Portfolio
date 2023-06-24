5480e0f74059aa95360cc0dae736f4f051100086
xkrzypandax <xkrzypandax@gmail.com>
2023-06-24 01:34:52 -0700

    Photo

--- src/components/About.js
+++ src/components/About.js
@@ -35,7 +35,7 @@ export default function About() {
                     <img
                         className="object-cover object-center rounded"
                         alt="hero"
-                        src="./coding.svg"
+                        src="./me.svg"
                     />
                 </div>
             </div>
