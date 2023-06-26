867dfe81b75ea871cf989739b601fcc1914a9fbd
xkrzypandax <xkrzypandax@gmail.com>
2023-06-25 17:55:56 -0700

    Tets

--- src/components/Thankyou.js
+++ src/components/Thankyou.js
@@ -1,17 +1,17 @@
 import React from "react";
 
-export default function Thankyou(){
+export default function Thankyou() {
     return (
-        <>
-            <h1> Joke to make you Laugh 😁 </h1>
-            <p align="center">
-                <img src="https://readme-jokes.vercel.app/api?theme=night-owl" alt="Jokes😁 Refresh the page for a New Joke"> 
-            </p>
-                <div align="center">
-                    <a href="https://git.io/typing-svg">
-                        <img alt="Thanks for your visit ❤️" src="https://readme-typing-svg.demolab.com?font=Roboto+Slab&size=15&pause=1000&color=00FF00&center=true&vCenter=true&width=435&lines=Thanks+for+your+visit+❤️" > 
-                    </a>
-                </div>
-        </>
+            <section id="Thankyou">
+                <h1> Joke to make you Laugh 😁 </h1>
+                <p align="center">
+                    <img src="https://readme-jokes.vercel.app/api?theme=night-owl" alt="Jokes😁 Refresh the page for a New Joke"> 
+                </p>
+                    <div align="center">
+                        <a href="https://git.io/typing-svg">
+                            <img alt="Thanks for your visit ❤️" src="https://readme-typing-svg.demolab.com?font=Roboto+Slab&size=15&pause=1000&color=00FF00&center=true&vCenter=true&width=435&lines=Thanks+for+your+visit+❤️" > 
+                        </a>
+                    </div>
+            </section>
     );
 }
