b49fe10c89e8c78470fdd2042e8b8dd81d37e1c4
xkrzypandax <xkrzypandax@gmail.com>
2023-06-25 17:36:33 -0700

    Thank you

---
+++ src/components/Thankyou.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Thankyou() {
+    return(
+        <h1> Joke to make you Laugh 😁 </h1>
+        <p align="center">
+        <img src="https://readme-jokes.vercel.app/api?theme=night-owl" alt="Jokes😁 Refresh the page for a New Joke"> </p>
+        <div align="center">
+        <a href="https://git.io/typing-svg">
+        <img alt="Thanks for your visit ❤️" src="https://readme-typing-svg.demolab.com?font=Roboto+Slab&size=15&pause=1000&color=00FF00&center=true&vCenter=true&width=435&lines=Thanks+for+your+visit+❤️" > </a>
+        </div>
+    );
+}
