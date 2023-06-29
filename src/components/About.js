919d801df3965e903d1c25ca9eba6e196d120113
xkrzypandax <xkrzypandax@gmail.com>
2023-06-28 17:16:18 -0700

    Grammar

--- src/components/About.js
+++ src/components/About.js
@@ -27,7 +27,7 @@ export default function About() {
                             href="#projects"
                             className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded text-lg"
                         >
-                            My Past Work
+                            My Past Works
                         </a>
                     </div>
                 </div>
